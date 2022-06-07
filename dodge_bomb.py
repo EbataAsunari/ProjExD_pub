167737525dca74175a003d43d41ff45cc56e81de
Andr0medapants <85671235+Andr0medapants@users.noreply.github.com>
2022-06-07 18:07:26 +0900

    #10

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -135,18 +135,11 @@ def check_bound(sc_r, obj_r):
 def increase_bombs(bombs, screen):
     global time_count
     time_now = time.time()
+    color = ((255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0))
     if ((time_now-time_count) > 5):
         #福光　ランダムに色と大きさが決定されるように
         for i in range(0, 6):
-            r = random.randint(0, 3)
-            if r == 0:
-                r = (255, 0, 0)
-            elif r == 1:
-                r = (0, 255, 0)
-            elif r == 2:
-                r = (0, 0, 255)
-            elif r == 3:
-                r = (255, 255, 0)
+            r = color(random.randint(0, 3))
         bombs.add( Bomb(r, random.randint(10, 50), (+2, +2), screen) )
         time_count = time_now
 
